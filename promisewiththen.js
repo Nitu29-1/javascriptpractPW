51b7aa18256a851e46a206b9f5141c03f15228fd
Nitu Nitu <nitupanjiyarplaywright@gmail.com>
2025-06-29 00:49:15 +0530

    javascript practice for PW

---
+++ promisewiththen.js
@@ -0,0 +1,26 @@
+function loginUser() {
+   return new Promise((resolve) => {
+       setTimeout(() => resolve("Logged in as Sidharth"), 6000);
+   });
+}
+function fetchDashboard() {
+   return new Promise((resolve) => {
+       setTimeout(() => resolve("Dashboard loaded"), 3000);
+   });
+}
+function fetchVoid() {
+   console.log("Void function called");
+}
+
+function runApp() {
+   loginUser()
+      .then((loginMessage) => {
+         console.log(loginMessage);
+         return fetchDashboard();
+      })
+      .then((dashboard) => {
+         console.log(dashboard);
+      })
+}
+runApp();
+fetchVoid();
